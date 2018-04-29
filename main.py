86cfa7e1fac74b574295b992fdef9aaaf4950ade
Mauricio Alvim <malvimmacedo@gmail.com>
2018-04-29 00:28:52 -0300

    Day 3: the bar is moving.

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
+import os
 import pygame
 from pygame.locals import *
 
+IMAGES_PATH = os.path.join(".", "images")
+
 class Background:
-    surface = None
+    image = None
 
     def __init__(self):
         screen = pygame.display.get_surface()
-        self.surface = pygame.Surface(screen.get_size()).convert()
-        self.surface.fill((100, 50, 20))
+        self.image = pygame.Surface(screen.get_size()).convert()
+        self.image.fill((100, 50, 20))
         
     def draw(self, screen):
-        screen.blit(self.surface, (0, 0))
+        screen.blit(self.image, (0, 0))
 
 
 class GameObject(pygame.sprite.Sprite):
+    def __init__(self, position):
+        pygame.sprite.Sprite.__init__(self)
+        self.set_position(position)
+
     def update(self):
         pass
 
-    def draw(self):
-        pass
+    # def draw(self, screen):
+    #     screen.blit(self.image, self.position)
 
     def get_speed(self):
         return self.speed
@@ -34,12 +41,28 @@ class GameObject(pygame.sprite.Sprite):
 
 
 class Player(GameObject):
-    def draw(self):
-        pass
+    image = None
+    rect = None
+
+    def __init__(self):
+        GameObject.__init__(self, (0, 0))
+        screen = pygame.display.get_surface()
+        screen_size = screen.get_size()
+        self.image = pygame.Surface((screen_size[0]/10, screen_size[1]/30)).convert()
+        self.image.fill((255, 255, 255))
+        self.rect = self.image.get_rect()
+        self.rect.bottom = 600
+        self.rect.centerx = 400
+
+    def update(self, dt):
+        self.rect.centerx += 25
+        if self.rect.left > pygame.display.get_surface().get_size()[0]:
+            self.rect.right = 0
 
 class Game:
     run = True
     background = None
+    all_sprites = pygame.sprite.RenderUpdates()
 
     def __init__(self):
         pygame.init()
@@ -52,23 +75,33 @@ class Game:
                 self.run = False
 
     def actors_update(self, dt):
-        pass
+        self.all_sprites.update(dt)
 
     def actors_draw(self):
-        self.background.draw(self.screen)
+        self.all_sprites.clear(self.screen, self.background.image)
+        rectlist = self.all_sprites.draw(self.screen)
+        pygame.display.update(rectlist)
+
+    def play(self):
+        player = Player()
+        self.all_sprites.add(player)
 
     def loop(self):
         dt = 40
         FPS = 1000 / dt
         clock = pygame.time.Clock()
+        self.background.draw(self.screen)
+        pygame.display.update()
+        self.play()
+
+        print(self.screen)
 
         while self.run:
             clock.tick(FPS)
             self.handle_events()
             self.actors_update(dt)
             self.actors_draw()
-            print('FPS: {0:.2f}'.format(clock.get_fps()))
-            pygame.display.flip()
+            # print('FPS: {0:.2f}'.format(clock.get_fps()))
 
 
 def main():
